c7095b245cc0a460f1e8f0988419ad7197008694
Matthew Meredith <matthewmeredith775@gmail.com>
2022-09-04 03:12:38 -0400

    module-ify and more test cases

--- misc/equationparser.py
+++ misc/equationparser.py
@@ -1,18 +1,18 @@
 import dataclasses
 from typing import Any
 
-from misc import pyparse as p
-# Define our parsers
-from misc.pyparse import ParserState
+from misc.pyparse import parsers as p
+from misc.pyparse import mapping as m
 
-# We're still debugging
-# p.do_debugging = True
+# Define our parsers
+
+from misc.pyparse.pyparse import ParserState
 
 __operation = p.string(('*', '/', '+', '-', '%', '^'))
 
 __numerical_constant = p.regex('([0-9]|e|pi)+')
 
-__parenthesis = p.lazy(lambda: __statement.between(p.string('('), p.string(')')))
+__parenthesis = p.many(p.lazy(lambda: __statement.between(p.string('('), p.string(')')))).map(m.singleton_list_unpacker)
 
 __expression = p.any_of(
     # Recursively add parenthesis, which contain a value (which could be more parenthesis)
@@ -24,7 +24,7 @@ __expression = p.any_of(
         # you can but a number next to parenthesis for *
         # 4(4) = 4 * 4
         p.optional(__parenthesis)
-    ).map(lambda x: x[0] if len(x) == 1 else x)
+    ).map(m.singleton_list_unpacker)
     # If only a value is matched, remove the list around it
 )
 
@@ -32,58 +32,75 @@ __expression = p.any_of(
 __statement = p.seperated_by(__operation, __expression)
 
 
-# Do some math
+# Define the evaluator
+
+def evaluate(node: Any):
+    pass
+
+
+# TESTING
 
 
 @dataclasses.dataclass
-class EquationTest:
+class TestCase:
     equation: str
     expected_result: Any
+    expected_value: int
     furthest_index: int = -1
 
 
-def test_equation(equation: EquationTest) -> ParserState:
-    final_state = __statement.run(test.equation.replace(' ', ''))
+def test_equation(case: TestCase) -> ParserState:
+    final_state = __statement.run(case.equation.replace(' ', ''))
 
-    if test.furthest_index < 0:
+    if case.furthest_index < 0:
         # Must match full
         assert not final_state.is_incomplete_match(), \
             "Expected complete match, only matched up to {} ({} < {})".format(final_state.furthest_index,
                                                                               final_state.index,
                                                                               len(final_state.target))
     else:
-        assert final_state.furthest_index == test.furthest_index, \
-            "Expected match up to {}, actually matched up to {}".format(test.furthest_index,
+        assert final_state.furthest_index == case.furthest_index, \
+            "Expected match up to {}, actually matched up to {}".format(case.furthest_index,
                                                                         final_state.furthest_index)
 
-    assert test.expected_result == final_state.result, \
+    assert case.expected_result == final_state.result, \
         """Results did not match expected
         Expected: `{}`
-        Actual  : `{}`""".format(test.expected_result, final_state.result)
+        Actual  : `{}`""".format(case.expected_result, final_state.result)
 
     return final_state
 
 
 # I don't like math
 tests = [
-    EquationTest("1", ['1'], -1),
-    EquationTest("1+1", ['1', '+', '1'], -1),
-    EquationTest("1+(1-2)", ['1', '+', ['1', '-', '2']], -1),
-    EquationTest("12 + 13 * (12 + (41 - (698) + 23 ^ 90 / (123epi ^ 2)) / 921)",
-                 ['12', '+', '13', '*',
-                  ['12', '+',
-                   ['41', '-',
-                    ['698'], '+', '23', '^', '90', '/',
-                    ['123epi', '^', '2']
-                    ],
-                   '/', '921']],
-                 -1)
+    TestCase(equation="1",
+             expected_result=['1'], expected_value=1,
+             furthest_index=-1),
+    TestCase(equation="1+1",
+             expected_result=['1', '+', '1'], expected_value=2,
+             furthest_index=-1),
+    TestCase(equation="1+(1-2)",
+             expected_result=['1', '+', ['1', '-', '2']], expected_value=0,
+             furthest_index=-1),
+    TestCase(equation="12 + 13 * (12 + (41 - (698) + 23 ^ 90 / (123epi ^ 2)) / 921)",
+             expected_result=[
+                     '12', '+', '13', '*',
+                     [
+                         '12', '+',
+                         [
+                             '41', '-', ['698'], '+', '23', '^', '90', '/',
+                             ['123epi', '^', '2']
+                         ],
+                         '/', '921'
+                     ]
+                 ], expected_value=2,
+             furthest_index=-1)
 ]
-
-# p.do_debugging = True
 
 # Parse our math stuff with
 for idx, test in enumerate(tests):
+    p.setup_debugging(False)
+
     print("\n###########################\n")
     print("Test {}/{}".format(idx + 1, len(tests)))
 
@@ -101,7 +118,8 @@ for idx, test in enumerate(tests):
 
     print()
 
-    print("Maximum Call Depth:", p.max_call_depth, "method calls")
+    print("Maximum Call Depth   :", p.max_call_depth, "method calls")
+    print("Total Transformations:", p.ParserState.total_transformations)
 
 print("\n###########################\n")
 print("All tests passed!")
